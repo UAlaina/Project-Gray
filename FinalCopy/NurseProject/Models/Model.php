d4c46ae88a1f2422c13c43249bc42d87c5de360c
UAlaina <urmicaa@gmail.com>
2025-04-20 14:30:48 -0400

    final copy i think

---
+++ FinalCopy/NurseProject/Models/Model.php
@@ -0,0 +1,21 @@
+<?php
+class Model{
+    
+    public static function connect(){
+        $server = "localhost";
+        $user = "root";
+        $pass = "";
+        $db = "nurserysystem";
+        $host = 3307;
+
+        $conn = new mysqli($server,$user, $pass, $db, $host);
+
+        if($conn->connect_error){
+            die("Connection error! I can't deal with anymore<br>" . $conn->connect_error);
+
+        }
+        
+        return $conn;
+    }
+}
+?>
